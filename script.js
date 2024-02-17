1803d20f4a775b63f22680d15dd8d5eee2bfdf46
AlexandraMadalinaG <madalinagrigore62@yahoo.com>
2024-02-17 19:14:16 +0000

    commit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var content = document.getElementsByTagName('body')[0];
+var darkMode = document.getElementById('dark-change');
+darkMode.addEventListener('click', function(){
+    darkMode.classList.toggle('active');
+    content.classList.toggle('night');
+});
+
 function toggleMenu() {
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
